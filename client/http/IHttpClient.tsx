448c2b863bac529157c659970090b5bc98253dce
Nikola Bozhkov <bozhkov.nikola@gmail.com>
2021-05-28 15:43:37 +0300

    Remove unused http client interface

--- client/http/IHttpClient.tsx
+++
@@ -1,6 +0,0 @@
-export interface IHttpClient {
-  get<T>(apiUrl: string, body?: unknown): Promise<T>;
-  post<T>(apiUrl: string, body: unknown): Promise<T>;
-  put<T>(apiUrl: string, body: unknown): Promise<T>;
-  delete<T>(apiUrl: string, body: unknown): Promise<T>;
-}
